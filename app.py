df13613be51d98ce1e92070a617db969769a4cc4
Haryson Knaak <haryson.plaster@aluno.senai>
2024-10-10 11:50:54 -0300

    10/10

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import pandas as pd
-#from openpyxl import load_workbook
+
 """
+from openpyxl import load_workbook
+
 # Carregar a planilha
 workbook = load_workbook('planilha.xlsx')
 worksheet = workbook.active  # Selecionando a primeira planilha
@@ -19,18 +21,16 @@ df = pd.read_excel('planilha.xlsx')
 # Removendo todos os espaços em branco de uma coluna
 df['Date'] = df['Date'].str.replace('\s+', '|', regex=True)
 
-"""
-df_2021 = df['Date'].str.split('|')[2] == 2021
-df['ano'] = df['Date'].str.split('|')
-df['ano'] = df['ano'][2]
-"""
-
+#Quebrando a coluna 'Date' onde tiver '|' e pegando apenas o último valor, e criando a coluna 'ano' para armazenar este registro
 df['ano'] = df['Date'].str.split('|').apply(lambda x: x[-1])
-print(df)
 
-#print(df)
-#Formatar os dados
+#Filtrando LINHAS à partir da coluna 'ano'
+df = df[df['ano'].apply(lambda x: int(x)) > 2020]
+
+#Filtrando COLUNAS
 #df = df[['Date', 'Open', 'Close']]  # Selecionar apenas essas colunas
+
+print(df)
 
 """
 # Salvando como CSV com personalizações
