205dcf4b588cae789a46d4afe6b530390bee2ce8
Haryson Knaak <haryson.plaster@aluno.senai>
2024-10-18 08:59:11 -0300

    Sextou!

--- app.py
+++ app.py
@@ -14,20 +14,25 @@ with open('teste.txt', 'w') as f:
         f.write(line + '||\n')
 """
 
+#Condicional para criar as planilhas
+create = False
 
 #Ler a planilha
 df = pd.read_excel('../MovimentaçãoMercantis_ORIGINAL.xlsx')
 df['CNPJ_ES'] = df['CNPJ_ES'].astype(str)
-df['CPF_CNPJ'] = df['CPF_CNPJ'].astype(str)
+df['CPF_CNPJ'] = df['CPF_CNPJ'].apply(lambda x: str(x) if pd.notnull(x) else x)
+
 
 desconto_devolucao = df[df['DESC_MERC_DEV'].apply(lambda x: float(x) != 0.0)] #Filtrando linhas
 df = df[df['DESC_MERC_DEV'].apply(lambda x: float(x) == 0.0)] #Excluindo linhas filtradas
-desconto_devolucao.to_excel('LINHAS_COM_DESC.POR_DEVOLUÇÃO_08.xlsx', index=False) #Criando tabela com as linhas filtradas
+if (create):
+    desconto_devolucao.to_excel('LINHAS_COM_DESC.POR_DEVOLUÇÃO_08.xlsx', index=False) #Criando tabela com as linhas filtradas
 
 #DS_CC ou DS_BANCO
 excluido_devolucao = df[df['DS_CC'].apply(lambda x: 'DEVOLUÇ' in x)] #Filtrando linhas
 df = df[df['DS_CC'].apply(lambda x: not('DEVOLUÇ' in x))] #Excluindo linhas filtradas
-excluido_devolucao.to_excel('LANCTO.EXCLUIDOS_EM_DEVOLUÇÃO_08.xlsx', index=False) #Criando tabela com as linhas filtradas
+if (create):
+    excluido_devolucao.to_excel('LANCTO.EXCLUIDOS_EM_DEVOLUÇÃO_08.xlsx', index=False) #Criando tabela com as linhas filtradas
 
 
 # Removendo todos os espaços em branco de uma coluna
@@ -46,6 +51,9 @@ excluido_devolucao.to_excel('LANCTO.EXCLUIDOS_EM_DEVOLUÇÃO_08.xlsx', index=Fal
 df.drop(columns=['VALOR_PRINCIPAL', 'VALOR_REC_PAGO'], inplace=True)
 
 print(df)
+
+df.to_excel('Movimentação_Mercantis_08.xlsx', index=False)
+
 """
 print(df.columns)
 print("-----------------")
