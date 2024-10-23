4c99f904454ce081bc244e2fe6454b393b1a7a8e
Haryson Knaak <harysonknaakplaster@gmail.com>
2024-10-23 12:03:29 -0300

    Zerar os valores em vez de retirar da planilha

--- app.py
+++ app.py
@@ -1,44 +1,74 @@
 import pandas as pd
 
-"""
-from openpyxl import load_workbook
-
-# Carregar a planilha
-workbook = load_workbook('planilha.xlsx')
-worksheet = workbook.active  # Selecionando a primeira planilha
-
-# Criar o arquivo TXT
-with open('teste.txt', 'w') as f:
-    for row in worksheet.iter_rows(values_only=True):
-        line = '|'.join(str(cell) for cell in row)  # Unindo as células com tabulação
-        f.write(line + '||\n')
-"""
-
-#Condicional para criar as planilhas
+# Condicional para criar as planilhas
 create = False
 
-#Ler a planilha
+# Ler a planilha
 df = pd.read_excel('../MovimentaçãoMercantis_ORIGINAL.xlsx')
+
+
+# Converter a coluna de data para o tipo datetime
+df['DATMOV'] = pd.to_datetime(df['DATMOV'])
+# Formatando a data para o formato brasileiro (DD/MM/AAAA)
+df['DATMOV'] = df['DATMOV'].dt.strftime('%d/%m/%Y')
+
+
 df['CNPJ_ES'] = df['CNPJ_ES'].astype(str)
 df['CPF_CNPJ'] = df['CPF_CNPJ'].apply(lambda x: str(x) if pd.notnull(x) else x)
+# df = df.applymap(lambda x: str(x) if pd.notnull(x) else x) # Método para aplicar função no DataFrame inteiro (apllymap)
 
-# Método para aplicar função no DataFrame inteiro (apllymap)
-#df = df.applymap(lambda x: str(x) if pd.notnull(x) else x)
+
+# Removendo colunas pelo nome
+df.drop(columns=['VALOR_PRINCIPAL', 'VALOR_REC_PAGO'], inplace=True)
+'''
+# Removendo colunas DINAMICAMENTE pelo nome
+dropColumns = ['VALOR_PRINCIPAL', 'VALOR_REC_PAGO']
+for column in df.columns:
+    if column in dropColumns:
+        df.drop(columns=[column], inplace=True)
+'''
+
+print("DataFrame Completo")
+print(df)
+print('--------------------------------------------------------------------------------------------------')
 
 
 desconto_devolucao = df[df['DESC_MERC_DEV'].apply(lambda x: float(x) != 0.0)] #Filtrando linhas
-df = df[df['DESC_MERC_DEV'].apply(lambda x: float(x) == 0.0)] #Excluindo linhas filtradas
+df_filtrado = df[df['DESC_MERC_DEV'].apply(lambda x: float(x) == 0.0)] #Excluindo linhas filtradas
 if (create):
     desconto_devolucao.to_excel('LINHAS_COM_DESC.POR_DEVOLUÇÃO_08.xlsx', index=False) #Criando tabela com as linhas filtradas
 
 #DS_CC ou DS_BANCO
 excluido_devolucao = df[df['DS_CC'].apply(lambda x: 'DEVOLUÇ' in x)] #Filtrando linhas
-df = df[df['DS_CC'].apply(lambda x: not('DEVOLUÇ' in x))] #Excluindo linhas filtradas
+df_filtrado = df[df['DS_CC'].apply(lambda x: not('DEVOLUÇ' in x))] #Excluindo linhas filtradas
 if (create):
     excluido_devolucao.to_excel('LANCTO.EXCLUIDOS_EM_DEVOLUÇÃO_08.xlsx', index=False) #Criando tabela com as linhas filtradas
 
-df_pagto = df[df['SAIDA'].apply(lambda x: x != 0 )] # Separando a tabela de PAGTO e RECEBTO
-df_recebto = df[df['ENTRADA'].apply(lambda x: x != 0 )] # Separando a tabela de PAGTO e RECEBTO
+
+df_pagto = df[df['SAIDA'].apply(lambda x: float(x) != 0.0 )] # Separando a tabela de PAGTO e RECEBTO
+df_recebto = df[df['ENTRADA'].apply(lambda x: float(x) != 0.0 )] # Separando a tabela de PAGTO e RECEBTO
+
+
+# print(df_pagto[['DATMOV', 'SAIDA', 'ENTRADA', 'NRO_NFE', 'HISTORICO']])
+# print(df_recebto[['DATMOV', 'SAIDA', 'ENTRADA', 'NRO_NFE', 'HISTORICO']])
+print("DataFrame Filtrado")
+print(df_filtrado)
+print('--------------------------------------------------------------------------------------------------')
+print("DataFrame Desconto por Devolução")
+print(desconto_devolucao)
+print('--------------------------------------------------------------------------------------------------')
+print("DataFrame Excluidos por Devolução")
+print(excluido_devolucao)
+print('--------------------------------------------------------------------------------------------------')
+print("DataFrame Pagamentos")
+print(df_pagto)
+print('--------------------------------------------------------------------------------------------------')
+print("DataFrame Recebimentos")
+print(df_recebto)
+print('--------------------------------------------------------------------------------------------------')
+
+
+#df.to_excel('Movimentação_Mercantis_08.xlsx', index=False)
 
 
 # Removendo todos os espaços em branco de uma coluna
@@ -46,33 +76,6 @@ df_recebto = df[df['ENTRADA'].apply(lambda x: x != 0 )] # Separando a tabela de 
 
 #Quebrando a coluna 'Date' onde tiver '|' e pegando apenas o último valor, e criando a coluna 'ano' para armazenar este registro
 #df['ano'] = df['Date'].str.split('|').apply(lambda x: x[-1])
-
-#Filtrando LINHAS à partir da coluna 'ano'
-#df = df[df['ano'].apply(lambda x: int(x)) > 2020]
-
-#Filtrando COLUNAS
-#df = df[['Date', 'Open', 'Close']]  # Selecionar apenas essas colunas
-
-#Removendo colunas pelo nome
-df.drop(columns=['VALOR_PRINCIPAL', 'VALOR_REC_PAGO'], inplace=True)
-
-print(df_pagto[['DATMOV', 'SAIDA', 'ENTRADA', 'NRO_NFE', 'HISTORICO']])
-print(df_recebto[['DATMOV', 'SAIDA', 'ENTRADA', 'NRO_NFE', 'HISTORICO']])
-
-#df.to_excel('Movimentação_Mercantis_08.xlsx', index=False)
-
-"""
-print(df.columns)
-print("-----------------")
-#Removendo colunas dinamicamente pelo nome
-for column in df.columns:
-    print(column)
-    if column == 'VALOR_PRINCIPAL' or column == 'VALOR_REC_PAGO':
-        df.drop(columns=[column], inplace=True)
-
-print("-----------------")
-print(df.columns)
-"""
 
 """
 # Salvando como CSV com personalizações
